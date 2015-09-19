8adf4da65c5559694d451be7fa82a1fe6ab8f9f7
adhikasp <adhika.setyap@gmail.com>
2015-09-19 12:03:22 +0700

    Implement translation and point printing

--- Shape.cpp
+++ Shape.cpp
@@ -1,5 +1,4 @@
 #include "Shape.h"
-#include <vector>
 
 namespace CraneTranformation
 {
@@ -7,12 +6,29 @@ namespace CraneTranformation
 	using System::Drawing::Point;
 
 	Shape::Shape(array<Point>^ shapePoint)
-	{
+	{						  
 		points = shapePoint;
 	}
 
 	array<Point>^ Shape::get_points()
 	{
 		return points;
+	}
+
+	void Shape::translate(Point^ delta)
+	{
+		for (int i = 0; i < points->Length; i++)
+		{
+			points[i].X += delta->X;
+			points[i].Y += delta->Y;
+		}
+	}
+
+	void Shape::printPoint()
+	{
+		for each(Point^ point in points)
+		{
+			System::Console::WriteLine(point->X + " " + point->Y);
+		}
 	}
 }
